986f6f2d938efbdd8ea5e70fabe80f614b94df6d
Lhr <16386214+LuoHuiRu@users.noreply.github.com>
2022-06-10 19:06:46 +0800

    添加 nessus 失败

---
+++ deprecated/Nessus/nessus-install-service.ps1
@@ -0,0 +1,11 @@
+﻿$params = @{
+  Name           = 'Nessus'
+  DisplayName    = 'Nessus'
+  BinaryPathName = '&NessusPath'
+  Description    = 'Tenable Nessus Network Security Scanner'
+  StartupType    = 'Manual'
+}
+
+New-Service @params
+
+
---
+++ deprecated/Nessus/nessus-uninstall-service.ps1
@@ -0,0 +1,1 @@
+﻿Remove-Service -Name 'Nessus'
